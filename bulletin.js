ed90cb364e678e90b42dfa84e1d5718307b6ce93
Frank Yao <yaofangjie2010@126.com>
2014-12-09 16:31:12 +0800

    upperbox ok

--- bulletin.js
+++ bulletin.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-	$('.upperbox').click(function(){
-		$('.smallbox').animate({left:"-=500px"}, 500);
-	})
+	$('.upperbox').hover(
+		function(){	//鼠标停留
+			$('.arrow').css({"display":"inline"});},
+		function(){ //鼠标离开
+			$('.arrow').css({"display":"none"});}
+	);
+
+	$('#leftarrow').click(function(){
+		//$('.showbox').append($('.smallbox').css("left"));
+		//alert($('.smallbox').css("left"));
+		if (!($('.smallbox').css("left") == "0px"))
+			$('.smallbox').animate({left:"+=500px"}, 500);
+	});
+
+	$('#rightarrow').click(function(){
+		//$('.showbox').append($('.smallbox').css("left"));
+		if (!($('.smallbox').css("left") == "-2000px"))
+			$('.smallbox').animate({left:"-=500px"}, 500);
+	});
+
 });
