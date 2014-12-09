f5494ebfa5f6f9c67f07fd5253e86f45aa478294
Frank Yao <yaofangjie2010@126.com>
2014-12-09 18:40:21 +0800

    want use eq

--- bulletin.js
+++ bulletin.js
@@ -7,21 +7,34 @@ $(document).ready(function(){
 		};
 
 	var onLeftArrowClick = function(){
-		//$(".showbox").append($(".smallbox").css("left"));
+		//$("#showbox").append($(".smallbox").css("left"));
 		//alert($(".smallbox").css("left"));
 		if (!($(".smallbox").css("left") == "0px")){
 			$("#leftarrow").unbind("click");
-			$(".smallbox").animate({left:"+=500px"}, 500, 
+			$(".smallbox").animate({left:"+=500px"}, 300, 
 			function(){$("#leftarrow").bind("click", onLeftArrowClick)});
+
+			$("#showbox").children().each(function(){//恢复到初始状态
+				$(this).css({"opacity":"0.3"});
+			});
+			$("#showbox").children().eq(1).css({"opacity":"1"}); //强调
 		}
 	};
 
 	var onRightArrowClick = function(){
-		//$(".showbox").append($(".smallbox").css("left"));
+		//$("#showbox").append($(".smallbox").css("left"));
 		if (!($(".smallbox").css("left") == "-2000px")){ //-2000px：视图片个数确定
 			$("#rightarrow").unbind("click"); //避免在转换中继续点击
-			$(".smallbox").animate({left:"-=500px"}, 500,
+			$(".smallbox").animate({left:"-=500px"}, 300,
 			function(){$("#rightarrow").bind("click", onRightArrowClick)});
+
+			$("#showbox").children().each(function(){
+				$(this).css({"opacity":"0.3"});
+			});
+
+			//var str = $(".smallbox").css("left");
+			//var count(str.substr(0, str.length - 2)));
+			$("#showbox").children().eq(1).css({"opacity":"1"});
 		}
 	};
 
