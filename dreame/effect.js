9782ff02e30749b1c2d508019a9cea3d47c97da6
Frank Yao <yaofangjie2010@126.com>
2014-12-09 22:14:21 +0800

    dreame ok

--- dreame/effect.js
+++ dreame/effect.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
 	var onMouseIn = function(){
 		//alert("Hello");
 		$(this).children().stop(true, true);
+
 		$(".imgBackground").fadeOut(300, function(){
-			$(".discover").animate({left: "0px"},100);
+			$(".discover").animate({left: "0px", opacity: "1"},100);
+			$(".price").animate({width: "50px", height: "21px", opacity: "1"},100);
 		});
 	};
 
 	var onMouseOut = function(){
 		$(this).children().stop(true, true);
-		$(".discover").animate({left: "-100%"},300, function(){
+
+		$(".price").animate({width: "0px", height: "0px", opacity: "0"},100);
+		$(".discover").animate({left: "-100%", opacity: "0"},300, function(){
 			$(".imgBackground").fadeIn(100);
 		});
 	};
