d8712f7225613d4ff4b2fe3aa128c0f9addd46ec
Frank Yao <yaofangjie2010@126.com>
2014-12-09 16:46:38 +0800

    fix bugs, unbind click func

--- bulletin.js
+++ bulletin.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
 	var onMouseIn = function(){	//鼠标停留
-			$('.arrow').css({"display":"inline"});
+			$(".arrow").css({"display":"inline"});
 		};
 	var onMouseOut = function(){ //鼠标离开
-			$('.arrow').css({"display":"none"});
+			$(".arrow").css({"display":"none"});
 		};
 
 	var onLeftArrowClick = function(){
-		//$('.showbox').append($('.smallbox').css("left"));
-		//alert($('.smallbox').css("left"));
-		if (!($('.smallbox').css("left") == "0px")){
-			$('.smallbox').animate({left:"+=500px"}, 500);
+		//$(".showbox").append($(".smallbox").css("left"));
+		//alert($(".smallbox").css("left"));
+		if (!($(".smallbox").css("left") == "0px")){
+			$("#leftarrow").unbind("click");
+			$(".smallbox").animate({left:"+=500px"}, 500, 
+			function(){$("#leftarrow").bind("click", onLeftArrowClick)});
 		}
-	}
+	};
 
 	var onRightArrowClick = function(){
-		//$('.showbox').append($('.smallbox').css("left"));
-		if (!($('.smallbox').css("left") == "-2000px")){
-			$('.smallbox').animate({left:"-=500px"}, 500);
+		//$(".showbox").append($(".smallbox").css("left"));
+		if (!($(".smallbox").css("left") == "-2000px")){ //-2000px：视图片个数确定
+			$("#rightarrow").unbind("click"); //避免在转换中继续点击
+			$(".smallbox").animate({left:"-=500px"}, 500,
+			function(){$("#rightarrow").bind("click", onRightArrowClick)});
 		}
-	}
+	};
 
-	$('.upperbox').hover(onMouseIn,	onMouseOut);
+	$(".upperbox").hover(onMouseIn,	onMouseOut);
 
-	$('#leftarrow').click(onLeftArrowClick);
+	$("#leftarrow").click(onLeftArrowClick);
 
-	$('#rightarrow').click(onRightArrowClick);
+	$("#rightarrow").click(onRightArrowClick);
 
 });
