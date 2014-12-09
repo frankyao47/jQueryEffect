865bcbaf5f5a1f2e541ac150ed2d39275c54c96c
Frank Yao <yaofangjie2010@126.com>
2014-12-09 18:48:59 +0800

    bottom also change

--- bulletin.js
+++ bulletin.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	var point = 0;
+	var pictureNum = 5;
 	var onMouseIn = function(){	//鼠标停留
 			$(".arrow").css({"display":"inline"});
 		};
@@ -9,32 +11,25 @@ $(document).ready(function(){
 	var onLeftArrowClick = function(){
 		//$("#showbox").append($(".smallbox").css("left"));
 		//alert($(".smallbox").css("left"));
-		if (!($(".smallbox").css("left") == "0px")){
-			$("#leftarrow").unbind("click");
-			$(".smallbox").animate({left:"+=500px"}, 300, 
-			function(){$("#leftarrow").bind("click", onLeftArrowClick)});
-
-			$("#showbox").children().each(function(){//恢复到初始状态
+		if (point > 0){
+			$(".smallbox").animate({left:"+=500px"}, 300);
+			point--;
+			$("#showbox").children().each(function(){
 				$(this).css({"opacity":"0.3"});
 			});
-			$("#showbox").children().eq(1).css({"opacity":"1"}); //强调
+			$("#showbox").children().eq(point).css({"opacity":"1"}); //强调
 		}
 	};
 
 	var onRightArrowClick = function(){
 		//$("#showbox").append($(".smallbox").css("left"));
-		if (!($(".smallbox").css("left") == "-2000px")){ //-2000px：视图片个数确定
-			$("#rightarrow").unbind("click"); //避免在转换中继续点击
-			$(".smallbox").animate({left:"-=500px"}, 300,
-			function(){$("#rightarrow").bind("click", onRightArrowClick)});
-
+		if (point < pictureNum - 1){ 
+			$(".smallbox").animate({left:"-=500px"}, 300);
+			point++;
 			$("#showbox").children().each(function(){
 				$(this).css({"opacity":"0.3"});
 			});
-
-			//var str = $(".smallbox").css("left");
-			//var count(str.substr(0, str.length - 2)));
-			$("#showbox").children().eq(1).css({"opacity":"1"});
+			$("#showbox").children().eq(point).css({"opacity":"1"}); //强调
 		}
 	};
 
